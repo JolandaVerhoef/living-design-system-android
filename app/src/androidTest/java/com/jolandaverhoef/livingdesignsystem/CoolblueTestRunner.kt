a3ed3e5ebcdb2b6be80e1a48ae31c20731addc17
Jolanda Verhoef <jolanda.verhoef@coolblue.nl>
2018-03-09 10:21:03 +0100

    Implement screenshot testing

---
+++ app/src/androidTest/java/com/jolandaverhoef/livingdesignsystem/CoolblueTestRunner.kt
@@ -0,0 +1,17 @@
+package com.jolandaverhoef.livingdesignsystem
+
+import android.os.Bundle
+import com.facebook.testing.screenshot.ScreenshotRunner
+import android.support.test.runner.AndroidJUnitRunner
+
+class CoolblueTestRunner : AndroidJUnitRunner() {
+    override fun onCreate(args: Bundle) {
+        ScreenshotRunner.onCreate(this, args)
+        super.onCreate(args)
+    }
+
+    override fun finish(resultCode: Int, results: Bundle) {
+        ScreenshotRunner.onDestroy()
+        super.finish(resultCode, results)
+    }
+}
--- app/src/androidTest/java/com/jolandaverhoef/livingdesignsystem/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/jolandaverhoef/livingdesignsystem/ExampleInstrumentedTest.kt
@@ -2,6 +2,9 @@ package com.jolandaverhoef.livingdesignsystem
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
+import android.view.LayoutInflater
+import com.facebook.testing.screenshot.Screenshot
+import com.facebook.testing.screenshot.ViewHelpers
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -18,7 +21,10 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.jolandaverhoef.livingdesignsystem", appContext.packageName)
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val inflater = LayoutInflater.from(appContext)
+        val view = inflater.inflate(R.layout.activity_main, null, false)
+        ViewHelpers.setupView(view).setExactWidthDp(300).layout()
+        Screenshot.snap(view).record()
     }
 }
